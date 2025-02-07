118c92c8fa8fe00fc316f41befade75092a72c0d
Toin-Paulo <antoniopaulosilva345@gmail.com>
2025-02-07 12:22:53 -0300

    modulo_aluno.py

--- modulo_aluno.py
+++ modulo_aluno.py
@@ -1,39 +1,33 @@
 import json
-import os
 
 # Nome do arquivo JSON que armazenará os dados
-FILE_NAME = "alunos.json"
+Arquivo = "alunos.json"
 
 # Carrega os dados do JSON, se o arquivo existir
 def carregar_dados():
-    if os.path.exists(FILE_NAME):
-        with open(FILE_NAME, "r", encoding="utf-8") as file:
-            return json.load(file)
-    return []
+    with open(Arquivo, "r", encoding="utf-8") as file:
+        return json.load(file)
 
 # Salva os dados no arquivo JSON
 def salvar_dados(alunos):
-    with open(FILE_NAME, "w", encoding="utf-8") as file:
+    with open(Arquivo, "w", encoding="utf-8") as file:
         json.dump(alunos, file, indent=4, ensure_ascii=False)
 
-# Adiciona um novo aluno
-def adicionar_aluno(nome, idade, curso):
-    alunos = carregar_dados()
-    novo_id = len(alunos) + 1
-    alunos.append({"id": novo_id, "nome": nome, "idade": idade, "curso": curso})
-    salvar_dados(alunos)
-    print(f"Aluno {nome} cadastrado com sucesso!")
-
-
 # Lista todos os alunos
-def listar_alunos():
+def listar_alunos(id,senha):
     alunos = carregar_dados()
     if not alunos:
         print("Nenhum aluno cadastrado.")
-        return None
-    else:
-        for aluno in alunos:
-            return aluno
+        return
+    for aluno in alunos:
+       if id == aluno["id"] and senha == aluno["senha"]:
+           return aluno["nome"]
+
+def pesqusar_aluno(id,senha):
+    alunos = carregar_dados()
+    for aluno in alunos:
+        if id == aluno["id"] and senha == aluno["senha"]:
+            return Trueluno
         
 def pesqusar_aluno(id,senha):
     alunos = carregar_dados()
