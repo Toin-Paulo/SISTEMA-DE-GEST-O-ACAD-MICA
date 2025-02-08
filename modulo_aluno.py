59526485a691f5667cb8fe238c3873e959d8280e
Toin-Paulo <antoniopaulosilva345@gmail.com>
2025-02-07 21:28:22 -0300

    Update modulo_aluno.py

--- modulo_aluno.py
+++ modulo_aluno.py
@@ -34,5 +34,23 @@ def pesqusar_aluno(id,senha):
     for aluno in alunos:
         if id == aluno["matricula"] and senha == aluno["senha"]:
             return True
-        
+
+
+def media_aluno():
+    alunos = carregar_dados()  # Função para carregar os dados dos alunos
+    for aluno in alunos:
+        for materia in aluno["materias"]:
+            media = (materia["nota1"] + materia["nota2"]) / 2  # Calculando a média
+            materia["media"] = media  # Adicionando a média diretamente na matéria
+        #aluno["materias_com_media"] = aluno["materias"]  # Adicionando a lista de matérias com as médias ao dicionário do aluno
+
+    return alunos  # Retorna a lista de alunos com as médias incluídas
+
+def salvar_dados(alunos):
+    with open("alunos.json", "w", encoding="utf-8") as file:
+        json.dump(alunos, file, indent=4, ensure_ascii=False)
+
+# Carrega, calcula as médias e salva os dados
+alunos_com_media = media_aluno()
+salvar_dados(alunos_com_media)
         
